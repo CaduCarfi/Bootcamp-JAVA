8888957a6772be0585bb7b48efdda8cce16680a2
CaduCarfi <carficadu@gmail.com>
2025-03-21 09:41:02 -0300

    exercicio smartTV

---
+++ Pasta/src/sistemaSmartTV/usuario.java
@@ -0,0 +1,29 @@
+package sistemaSmartTV;
+
+public class usuario {
+
+    public static void main(String[] args) throws Exception {
+
+        SmartTV smartTV = new SmartTV();
+
+        smartTV.diminuirVolume();
+        smartTV.diminuirVolume();
+        smartTV.diminuirVolume();
+        smartTV.aumentarVolume();
+        System.out.println("Volume Atual : " +smartTV.volume);
+
+        System.out.println("Canal Atual : " +smartTV.canal);
+        smartTV.mudarCanal(13);
+        System.out.println("Canal Atual : " +smartTV.canal);
+
+        System.out.println("TV Ligada ? " +smartTV.ligado);
+        System.out.println("Canal Atual : " +smartTV.canal);
+        System.out.println("Volume Atual : " +smartTV.volume);
+
+        smartTV.ligar();
+        System.out.println("Novo Status -> TV Ligada ? " +smartTV.ligado);
+
+        smartTV.desligar();
+        System.out.println("Novo Status -> TV Ligada ? " +smartTV.ligado);
+    }
+}
