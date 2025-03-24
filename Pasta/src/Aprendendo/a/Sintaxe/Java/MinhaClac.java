8b99491327e15d927cc0b17a1f40fd3289f7a9f9
CaduCarfi <carficadu@gmail.com>
2025-03-24 11:49:44 -0300

    aprendendo documentação e execução por prompt de comando

---
+++ Pasta/src/Aprendendo/a/Sintaxe/Java/MinhaClac.java
@@ -0,0 +1,8 @@
+package Aprendendo.a.Sintaxe.Java;
+
+public class MinhaClac {
+
+    public static void main(String[] args) {
+        System.out.println("Oi, fui executado pelo Terminal");
+    }
+}
