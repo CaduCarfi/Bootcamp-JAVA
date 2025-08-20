bb6f6d86ac4caaa8c7f1d0cce1b6b57ea3ad0cef
Carlos Eduardo Carfi <157553755+CaduCarfi@users.noreply.github.com>
2025-08-20 10:05:52 -0300

    Metodo Construtor

---
+++ PROGRAMACAO ORIENTADA A OBJETOS/Pessoa/src/SistemaCadastro.java
@@ -0,0 +1,9 @@
+public class SistemaCadastro {
+    public static void main(String[] args) {
+        Pessoa marcos = new Pessoa("Marcos", "123");
+
+        marcos.setEndereco("Rua das marias");
+
+        System.out.println(marcos.getNome() + "-" + marcos.getCpf());
+    }
+}
