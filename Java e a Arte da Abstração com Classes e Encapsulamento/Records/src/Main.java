a4a0eb63d9af65cea68383e15a2cc744cdeba6f2
Carlos Eduardo Carfi <157553755+CaduCarfi@users.noreply.github.com>
2025-09-04 11:48:13 -0300

    Testando Records

---
+++ Java e a Arte da Abstração com Classes e Encapsulamento/Records/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        var person = new Person("Joao", 12);
+        System.out.println(person);
+        System.out.println(person.name());
+
+    }
+}
